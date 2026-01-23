b16ce12b75e39c4fbe4b97853aede7f655d2fd38
deepanjali-codes <deepanjali3346@gmail.com>
2026-01-23 13:31:07 +0530

    Remove all non-Photo_Booth files

--- Minecraft/python/123.py
+++
@@ -1,7 +0,0 @@
-import re
-a = "This is my number 8652492170"
-# print(re.findall(r"\d{10}",a))  for phone numbers
-
-# print(re.findall(r"\w+",a))   for everything in string
-
-# print(re.findall(r"\s+",a))  for spaces and tabs
